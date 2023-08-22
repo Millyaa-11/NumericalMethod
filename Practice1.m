17b8bfee18f49adee7ba08fb892f3a54e1c022b4
chalita <chalitaes@gmail.com>
2023-08-23 03:16:59 +0700

    upload

---
+++ Practice1.m
@@ -0,0 +1,28 @@
+x = [1 2 3 4 5]
+y = x
+x + y
+x(3) + y(end)
+x = [1 2 3 7 5]
+y(end+1) = 6
+y(3) = []
+c1 = [1 2 3]
+c2 = [4 5 6]
+c3 = [7 8 9]
+A = [c1; c2; c3]
+B = A
+d = det(B)
+e = eig(B)
+a = (3:3:9)
+b = [3;6;9]
+a * b
+myMat = [b,b]
+myMat(1,2) + myMat(2,1)
+z = rand([2,2])
+numel(z)
+max(x)
+min(x)
+max(x,2)
+z = rand(1, 8)
+s = sort(z)
+C = randi(9,9)
+sum(C(:,[4,6]), 2)
